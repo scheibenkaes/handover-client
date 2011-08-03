852d31769091f3d08b55a97e1a73ecf2516c4282
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-08-03 22:13:20 +0200

    contrib added

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject handover-client "1.0.0-SNAPSHOT"
   :description "Handing over files. DAU approved."
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]])
