01409f89094f884865dfbdf0b608db67bb659e1d
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-08-03 22:12:27 +0200

    Added d-ds

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject handover-client "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Handing over files. DAU approved."
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]])
