ec894fc3702367203645f2e9cbffd45355d65608
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-08-14 23:28:12 +0200

    Added fns account-manager, with-connection, create-user

--- src/handover_client/connection.clj
+++ src/handover_client/connection.clj
@@ -1,12 +1,25 @@
 (ns handover-client.connection
   (:use [clojure.contrib.properties :only [get-system-property]])
-  (:import [org.jivesoftware.smack XMPPConnection])
+  (:import [org.jivesoftware.smack Connection XMPPConnection])
   (:use digest))
 
 (def connection (atom nil))
 
+(defmacro with-connection [con & body] 
+  `(do
+     ~@body
+     (.disconnect ~con)))
+
 (defn connect [host] 
   (doto (XMPPConnection. host) .connect))
+
+(defn account-manager [con] 
+  (when con (.getAccountManager con)))
+
+(defn create-user [connection username password] 
+  (if-let [am (account-manager connection)]
+    (try 
+      (do (.createAccount am username password) :ok) (catch Exception _ nil))))
 
 (defn create-id [in] 
   (->> in (digest "SHA1") (take 8) (apply str)))
