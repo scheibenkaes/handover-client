db833a0f8f43b6e0958e09e64269ef76516c163d
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-09-18 21:20:11 +0200

    added "Einen Fehler melden" to info

--- src/handover_client/info.clj
+++ src/handover_client/info.clj
@@ -4,11 +4,18 @@
 
 (def info-icon (icon-by-name "help-browser"))
 
+(def desktop (java.awt.Desktop/getDesktop))
+
 (defn open-url [u]
-  (-> (java.awt.Desktop/getDesktop) (.browse (java.net.URI. u))))
+  (-> desktop (.browse (java.net.URI. u))))
 
 (defn url-button [text url]
   (action :name text :handler (fn [_] (open-url url))))
+
+(defn action-button [text act]
+  (action :name text :handler (fn [_] (act))))
+
+(def uri (java.net.URI. "mailto:scheibenkaes+handover@googlemail.com"))
 
 (def info-panel
   (mig-panel :constraints ["" "[][][]"]
@@ -16,7 +23,8 @@
                ["Copyright 2011 Benjamin Klüglein" "span 3,growx,wrap"]
                [:separator "wrap"]
                [(url-button "Handover im Internet" "http://scheibenkaes.org/software") "growx,wrap"]
-               [(url-button "Lizenz (GPLv3)" "http://www.gnu.de/documents/gpl.de.html") "growx"]]))
+               [(url-button "Lizenz (GPLv3)" "http://www.gnu.de/documents/gpl.de.html") "growx,wrap"]
+               [(action-button "Einen Fehler melden" #(.mail desktop uri)) "growx,wrap"]]))
 
 (defn show-info-dialog []
   (-> (dialog :type :info :content info-panel :title "Informationen zu Handover" :resizable? false) pack! show!))
